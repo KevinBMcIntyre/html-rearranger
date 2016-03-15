66dd09efc9a4f3ba0bd25da2d8d8ac5305f6cd6a
Kevin McIntyre <kevin@Kevins-MacBook-Pro.local>
2016-03-15 16:39:08 -0400

    finished code

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var $body = $('body');
+var $h2 = $('<h2>');
+var $ul2 = $('<ul>');
+var $li = $('<li>');
+var $h1 = $('<h2>');
+var $ul1 = $('ul');
+var $ulli = $('ul li');
+
+
+
+$body.append($h2);
+$h2.html('Below ground veggies');
+$body.append($ul2);
+$body.append($h1);
+$h1.html('Above ground veggies');
+$body.append($ul1);
+
+
+$ulli.each(function () {
+  if ($(this).hasClass('below')) {
+  	$ul2.append($(this));
+  } else {
+  	$ul1.append($(this));
+  }
+});
